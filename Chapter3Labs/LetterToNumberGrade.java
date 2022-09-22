58fe6d4798a9c33b3c4b011a823f03568823e4fe
Katie Wang <kwang3@stu.naperville203.org>
2022-09-22 14:19:23 -0500

    re

---
+++ Chapter3Labs/LetterToNumberGrade.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class LetterToNumberGrade here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.Scanner;
+
+public class LetterToNumberGrade
+{
+   public static void main(String args[]){
+        int highestNumber = 4;
+        String letterGrade;
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.print("Enter the letter grade (along with plus or minus if it has that) ");
+        letterGrade = input.nextLine();
+        
+        /*
+           B- = 2.7
+           B = 3
+           B = 3.3
+           A- = 3.7
+    
+           */
+        
+        
+        
+    
+    }
+}
