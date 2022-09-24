5c38fc62ae2ea576b39d28f8aee6d253bd979fe5
Katie Wang <kwang3@stu.naperville203.org>
2022-09-24 09:39:40 -0500

    sdf

--- Chapter3Labs/LetterToNumberGrade.java
+++ Chapter3Labs/LetterToNumberGrade.java
@@ -13,6 +13,7 @@ public class LetterToNumberGrade
    public static void main(String args[]){
         int highestNumber = 4;
         String letterGrade;
+        double numberGrade = 0;
         Scanner input = new Scanner(System.in);
         
         
@@ -20,13 +21,52 @@ public class LetterToNumberGrade
         letterGrade = input.nextLine();
         
         /*
+         * 
+         * F = 0
+         * D- = 0.7
+         * D = 1
+         * D+ = 1.3
+         * C- = 1.7
+           C = 2 
+           C+ = 2.3
            B- = 2.7
            B = 3
-           B = 3.3
-           A- = 3.7
+           B+ = 3.3
+           A- = 3.4
+           A = 3.7
+           A+ = 4.0
     
            */
+          
+        if (letterGrade.equals("F")){
+            numberGrade = 0;
+        }else if (letterGrade.equals ("D-")){
+            numberGrade = 0.7;
+        }else if (letterGrade.equals("D")){
+            numberGrade = 1;
+        }else if (letterGrade.equals("D+")){
+            numberGrade = 1.3;
+        }else if (letterGrade.equals("C-")){
+            numberGrade = 1.7;
+        }else if (letterGrade.equals("C")){
+            numberGrade = 2;
+        }else if (letterGrade.equals("C+")){
+            numberGrade = 2.3;
+        }else if (letterGrade.equals("B-")){
+            numberGrade = 2.7;
+        }else if (letterGrade.equals("B")){
+            numberGrade = 3;
+        }else if (letterGrade.equals("B+")){
+            numberGrade = 3.3;
+        }else if (letterGrade.equals("A-")){
+            numberGrade = 3.4;
+        }else if (letterGrade.equals("A")){
+            numberGrade = 3.7;
+        }else if (letterGrade.equals("A+")){
+            numberGrade = 4;
+        }
         
+        System.out.println("The number grade equivalent is " + numberGrade);
         
         
     
---
+++ Chapter3Labs/Triangle.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Triangle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Triangle
+{
+  public static void main(String args[]){
+    
+    
+    
+    
+    
+    
+    
+    }
+}
