74a10a1b0d6e4b33eb54da19de7e49ccc2c52ceb
Katie Wang <kwang3@stu.naperville203.org>
2022-09-24 22:56:23 -0500

    v

---
+++ Chapter3Labs/StupidCar.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class StupidCar here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.Scanner;
+
+public class StupidCar
+{
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        String carInputs;
+        
+        System.out.println("Car mechanism order: DL. DR. CL, MU, IL, IR, OL, OR, Gear");
+        System.out.println("Enter the car mechanisms in this order.\n0 for off, 1 for activated\nGear options are P N D 1 2 3 R");
+        
+        
+        //use indexing
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
