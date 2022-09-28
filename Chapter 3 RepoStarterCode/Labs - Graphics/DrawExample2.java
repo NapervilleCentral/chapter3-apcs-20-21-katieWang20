2420aa18be27d90b8cefb1d7f9ec91cf904712cf
Katie Wang <kwang3@stu.naperville203.org>
2022-09-28 13:41:50 -0500

    f

---
+++ Chapter 3 RepoStarterCode/Labs - Graphics/DrawExample2.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class DrawExample2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+
+public class DrawExample2
+{
+    // instance variables - replace the example below with your own
+   
+}
