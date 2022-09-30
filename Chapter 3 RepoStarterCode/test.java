4833d7d514773a7197a3713dbdee18acd93f859c
Katie Wang <kwang3@stu.naperville203.org>
2022-09-29 20:39:56 -0500

    dsfdsdfs

---
+++ Chapter 3 RepoStarterCode/test.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class test
+{
+   
+    public static void main(String args[]){
+  
+        // Initializing Strings
+        String str1 = "geeks";
+        String str2 = "Geeks";
+  
+        // Checking if geeksforgeeks string
+        // equates to geeksforgeeks string
+        // case sensitive
+        System.out.print(
+            "Difference of geeks and Geeks (case sensitive) : ");
+        System.out.println(str2.compareTo(str1));
+  
+        // Checking if geeksforgeeks string
+        // equates to astha string
+        // case insensitive
+        // using compareToIgnoreCase()
+        System.out.print(
+            "Difference of geeks and gEEkS (case insensitive) : ");
+        System.out.println(str1.compareToIgnoreCase(str2));
+    }
+}
+
