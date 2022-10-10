18c3dc98481f1a730dfca555a0a0cd96e166d64a
Katie Wang <kwang3@stu.naperville203.org>
2022-10-09 22:09:42 -0500

    ddd

---
+++ Chapter 3 RepoStarterCode/forlooptest.java
@@ -0,0 +1,100 @@
+/*here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+here
+
+
+cription of class forlooptest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class forlooptest
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args){
+        /*for (int x = 5; x > 0; x--){
+        
+        
+            for (int y = 0; y < 8; y++){
+                System.out.println(x*y);
+            }
+        
+        double a = 0.5;
+        a += 1.2;
+        
+        System.out.println(a);
+    }
+        
+    */
+   
+    for (int val = 200; val >=0; val -=1)
+        if (val % 4 !=0)
+            System.out.println(val);
+    }
+}
