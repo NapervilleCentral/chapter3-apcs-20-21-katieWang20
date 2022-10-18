4e18aef67f3d908c526b632ab7ae1003bcf9d240
Katie Wang <kwang3@stu.naperville203.org>
2022-10-18 14:19:31 -0500

    u

--- Chapter3B Starter Code/MontyHall.java
+++ Chapter3B Starter Code/MontyHall.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.lang.Math;
 
 
 public class MontyHall
@@ -10,16 +11,53 @@ public class MontyHall
         int strategy2Count = 0;
         final int NUMBER_OF_RUNS = 1000;
         Random generator = new Random();
-      
+       
         
       
         for (int y = 1; y<=NUMBER_OF_RUNS; y+=1){
             
-            int prizeDoor = generator.nextInt(4);
-            prizeDoor = prizeDoor + 1;
+            int prizeDoor = generator.nextInt(3) + 1;
             System.out.println(prizeDoor);
-            int personChoice = generator.nextInt(4) + 1;
-            int personChoice2;
+            int doorChoice1 = generator.nextInt(3) + 1;
+            int stick = 0; // 0 true, 1 is false
+            int doorChoice2 = 0;
+            System.out.println();
+            
+            if (doorChoice1 != prizeDoor){
+                
+                
+                System.out.println("Choose or stick");
+                int newDoorChoice = generator.nextInt(3)+1;
+                
+                if (doorChoice1 == newDoorChoice){
+                    strategy2Count +=1;
+                    System.out.println ("stick");
+                    System.out.println(); 
+                    
+                    if (newDoorChoice == prizeDoor){
+                        System.out.println("prizedoor" + prizeDoor + " " + newDoorChoice);
+                    }else{
+                        System.out.println("You chose door" + newDoorChoice + ". It is not door " + prizeDoor);
+                        
+                    }
+                    
+                    
+                
+                }else{
+                    strategy2Count +=1; 
+                    System.out.println("new remaining door chosen");
+                    if (newDoorChoice == prizeDoor){
+                        System.out.println("prizedoor" + prizeDoor + " " + newDoorChoice);
+                    }else{
+                        System.out.println("You chose door" + newDoorChoice + ". It is not door " + prizeDoor);
+                        
+                    }
+                }
+                    
+                
+            
+            } 
+            
             
                 
         
