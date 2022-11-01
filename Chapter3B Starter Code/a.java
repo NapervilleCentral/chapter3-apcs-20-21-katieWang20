fce0c86f0e58e1c73515d07332abbe96e466d267
Katie Wang <kwang3@stu.naperville203.org>
2022-11-01 18:31:37 -0500

    k

---
+++ Chapter3B Starter Code/a.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a description of class a here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class a
+{
+    // instance variables - replace the example below with your own
+    
+    public static void main (String[] args){
+        /*int b = 0;
+        int i = 1;
+        int a =0;
+        for (a = 0; a < 10; a++){
+            for (; b < 10; b++){
+                i +=b;
+                System.out.println(i);
+        
+            }
+            System.out.println();
+        }
+         
+        
+        int x = 10;
+        int y = 3;
+        boolean b = true;
+        for (int i = 0; i < 15 ; i += 5)
+        {
+          x = x + y;
+          b = ( x % y == 2);
+          System.out.println(b);
+          if (!b)
+          {
+            y++;
+            i+=5;
+          }
+          System.out.println(y + "  " + i);
+          System.out.println(x);
+        }
+        */
+        
+        for(int g= 6 ; g<17; g++)
+        {
+        System.out.println( g*2 + " ");
+    }
+    }
+}
