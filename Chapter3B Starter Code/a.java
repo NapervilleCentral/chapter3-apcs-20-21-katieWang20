cd5859cc562cd44412a2743c86e49fd1a9fd8738
Katie Wang <kwang3@stu.naperville203.org>
2022-11-01 21:15:54 -0500

    trhgy

--- Chapter3B Starter Code/a.java
+++ Chapter3B Starter Code/a.java
@@ -10,40 +10,27 @@ public class a
     // instance variables - replace the example below with your own
     
     public static void main (String[] args){
-        /*int b = 0;
-        int i = 1;
-        int a =0;
-        for (a = 0; a < 10; a++){
-            for (; b < 10; b++){
-                i +=b;
-                System.out.println(i);
-        
-            }
-            System.out.println();
-        }
-         
-        
-        int x = 10;
-        int y = 3;
-        boolean b = true;
-        for (int i = 0; i < 15 ; i += 5)
-        {
-          x = x + y;
-          b = ( x % y == 2);
-          System.out.println(b);
-          if (!b)
-          {
-            y++;
-            i+=5;
-          }
-          System.out.println(y + "  " + i);
-          System.out.println(x);
-        }
-        */
-        
-        for(int g= 6 ; g<17; g++)
-        {
-        System.out.println( g*2 + " ");
-    }
+        String temp = "Mississippi";
+
+String part = "si";
+
+ 
+int position = 0;
+
+int count = 0;
+
+while(temp.indexOf(part) >= 0)
+
+{
+
+   position = temp.indexOf(part);
+
+   result++;
+
+   temp = temp.substring(position + 1);
+
+}
+
+System.out.println(count);
     }
 }
